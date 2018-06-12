c1577aab5c14a127f73da04454d76306d7fed424
Jimmy S <jpsietsma@gmail.com>
2018-06-12 11:13:26 -0400

    Initial commit

--- fameUploadConsole/Program.cs
+++ fameUploadConsole/Program.cs
@@ -3,13 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
 
 namespace fameUploadConsole
 {
+
     class Program
     {
-        static void Main(string[] args)
+        public FileSystemWatcher fameWatcher = null;
+
+        //This method is called when a File Creation is detected
+        public static void OnChanged(object source, FileSystemEventArgs e)
         {
+            LogEvent("New file has been added!", EventLogEntryType.Information);
+            Console.WriteLine("File added!");
+        }
+
+        //logs event to the Windows Event Log as event type notification
+        public static void LogEvent(string message)
+        {
+            string eventSource = "FAME Document Upload Watcher";
+            DateTime dt = new DateTime();
+            dt = System.DateTime.UtcNow;
+            message = dt.ToLocalTime() + ": " + message;
+
+            EventLog.WriteEntry(eventSource, message);
+        }
+
+        //logs event to the windows event log as a specific event type
+        public static void LogEvent(string message, EventLogEntryType e)
+        {
+            string eventSource = "FAME Document Upload Watcher";
+            DateTime dt = new DateTime();
+            dt = System.DateTime.UtcNow;
+            message = dt.ToLocalTime() + ": " + message;
+
+            EventLog.WriteEntry(eventSource, message, e);
+        }
+
+        public static void Main(string[] args)
+        {
+            FileSystemWatcher fameWatcher = new FileSystemWatcher(@"E:\projects\famedrop");
+
+            fameWatcher.IncludeSubdirectories = false;
+            fameWatcher.NotifyFilter = NotifyFilters.CreationTime | NotifyFilters.FileName | NotifyFilters.LastWrite;
+
+            //Register the different types of file system events to listen for, Created, Changed, Renamed, Deleted
+            fameWatcher.Created += new FileSystemEventHandler(OnChanged);
+
+            //This begins the actual file monitoring
+            fameWatcher.EnableRaisingEvents = true;
+
+            while (true)
+            {
+                Thread.Sleep(30000);
+            }
+
         }
     }
 }
